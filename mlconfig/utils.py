8a570d37a97dcb161c398e42ce0090adca8f017a
Marco Podda <marco.podda@di.unipi.it>
2019-07-14 16:15:12 +0200

    working

--- mlconfig/utils.py
+++ mlconfig/utils.py
@@ -3,9 +3,8 @@ from pathlib import Path
 
 
 def load_yaml(path):
-    path = Path(path)
-    return yaml.load(open(path, "r"), Loader=yaml.FullLoader)
+    return yaml.load(open(Path(path), "r"), Loader=yaml.FullLoader)
+
 
 def save_yaml(obj, path):
-    path = Path(path)
-    yaml.dump(obj, open(path, "w"))
+    yaml.dump(obj, open(Path(path), "w"))
