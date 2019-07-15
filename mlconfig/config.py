dae2b6df76ec8e5d0699e20a29898c31722af051
Marco Podda <marco.podda@di.unipi.it>
2019-07-15 13:24:51 +0200

    modified modelselectionconfig

--- mlconfig/config.py
+++ mlconfig/config.py
@@ -1,12 +1,6 @@
+import itertools
 from .utils import load_yaml, save_yaml
 
-
-ALLOWED_LENGTH_ERROR_MSG = """
-Found a mismatch in the allowed length.
-Allowed lengths are 1 and {max_allowed_length},
-but we found an item with length {current_length}.
-Check your configuration.
-"""
 
 NOT_A_LIST_ERROR_MSG = """
 The value associated with parameter '{key}' must be a list.
@@ -48,7 +42,6 @@ class Config(LoadMixin):
 
 class ModelSelectionConfig(LoadMixin):
     def __init__(self, **ms_dict):
-        self.max_allowed_length = 1
         self._validate(ms_dict)
         self._dict = ms_dict
 
@@ -58,27 +51,13 @@ class ModelSelectionConfig(LoadMixin):
                 msg = NOT_A_LIST_ERROR_MSG.format(key=name)
                 raise ConfigError(msg)
 
-        for name, value in ms_dict.items():
-            current_length = len(value)
-            if self.max_allowed_length != 1 and current_length != self.max_allowed_length:
-                msg = ALLOWED_LENGTH_ERROR_MSG.format(max_allowed_length=self.max_allowed_length,
-                                                      current_length=current_length)
-                raise ConfigError(msg)
-            self.max_allowed_length = current_length
-
-        for name, value in ms_dict.items():
-            if len(value) == 1:
-                ms_dict[name] = value * self.max_allowed_length
-
-    def _get_config_dict(self, index):
-        # We return dictionaries instead of Config objects,
-        # as doing so creates problems in case of parallelization.
-        # Config objects must be created inside the dedicated thread.
-        return {k: v[index] for (k, v) in self._dict.items()}
-
     def __iter__(self):
-        return iter(self._get_config_dict(i) for i in range(self.max_allowed_length))
-
-    def __getitem__(self, index):
-        return self._get_config_dict(index)
+        items = sorted(self._dict.items())
+        if not items:
+            yield {}
+        else:
+            keys, values = zip(*items)
+            for v in itertools.product(*values):
+                params = dict(zip(keys, v))
+                yield params
 
